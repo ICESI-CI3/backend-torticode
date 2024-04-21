9683fbbf6a5f772f508bb9bd17e7b6a83cddaddb
Dan-Ale-Es-A <alexandraarenas1705@gmail.com>
2024-04-20 21:26:33 -0500

    feat:dto_product_has_been_added

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,21 @@
-export class CreateProductDto {}
+import { IsInt, IsString, Length, IsOptional } from "class-validator";
+
+export class CreateProductDto {
+
+    @IsString()
+    readonly name: string;
+
+    @IsString()
+    @Length(10,150)
+    readonly description: string;
+
+    @IsInt()
+    readonly price: number;
+
+    @IsInt()
+    readonly stock: number;
+
+    @IsOptional()
+    readonly image: string;
+
+}
