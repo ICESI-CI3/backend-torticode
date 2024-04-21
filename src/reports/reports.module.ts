af8888c48486b35b2b5d2637dbcf52cb55cae276
Dan-Ale-Es-A <alexandraarenas1705@gmail.com>
2024-04-21 12:49:05 -0500

    fix:error_with_reports_imports_modules_has_been_fixed

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -3,9 +3,10 @@ import { ReportsService } from './reports.service';
 import { ReportsController } from './reports.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Report } from './entities/report.entity';
+import {Sale} from '../sales/entities/sale.entity';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([Report])],
+  imports:[TypeOrmModule.forFeature([Report,Sale])],
   controllers: [ReportsController],
   providers: [ReportsService],
 })
