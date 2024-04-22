c5084ead041f212c78ed6b58983bbdd761a31d70
DianaLBS <dianalorenabalanta@gmail.com>
2024-04-22 18:41:02 -0500

    feat:update_sale-details

--- src/sale-details/dto/create-sale-detail.dto.ts
+++ src/sale-details/dto/create-sale-detail.dto.ts
@@ -1,1 +1,12 @@
-export class CreateSaleDetailDto {}
+import { IsInt, IsNotEmpty, IsNumber, IsPositive } from "class-validator";
+
+export class CreateSaleDetailDto {
+    @IsNotEmpty()
+    @IsNumber()
+    productId: number;
+    
+    @IsNotEmpty()
+    @IsInt()
+    @IsPositive()
+    quantity: number;
+}
--- src/sales/sales.service.ts
+++ src/sales/sales.service.ts
@@ -4,18 +4,31 @@ import { UpdateSaleDto } from './dto/update-sale.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Sale } from './entities/sale.entity';
+import { SaleDetail } from 'src/sale-details/entities/sale-detail.entity';
+import { Student } from 'src/roles/entities/student.entity';
+import { Restaurant } from 'src/roles/entities/restaurant.entity';
+import { Status } from './enum/status.enum';
 
 @Injectable()
 export class SalesService {
   constructor(
     @InjectRepository(Sale) 
-    private saleRepository: Repository<Sale>
+    private saleRepository: Repository<Sale>,
+    @InjectRepository(SaleDetail)
+    private saleDetailRepository: Repository<SaleDetail>,
+    @InjectRepository(Student)
+    private studentRepository: Repository<Student>,
+    @InjectRepository(Restaurant)
+    private restaurantRepository: Repository<Restaurant>,
   ) {}
 
-  async create(createSaleDto: CreateSaleDto) {
-    const sale = this.saleRepository.create(createSaleDto); //Crea un objeto en memoria
-    return await this.saleRepository.save(sale); //Guarda el objeto en bd
-  }
+  async create(createSaleDto: CreateSaleDto): Promise<Sale> {
+    const sale = new Sale();
+    sale.restaurant = await this.restaurantRepository.findOne({where:{id:createSaleDto.restaurantId}});
+    sale.student = await this.studentRepository.findOne({where:{id:createSaleDto.studentId}});
+    sale.status = Status.PENDING;
+    return this.saleRepository.save(sale);
+}
 
   async findAll() {
     return await this.saleRepository.find();
