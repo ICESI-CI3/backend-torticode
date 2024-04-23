0753586af67942aa7346dda247abbe69da1fa4ad
DianaLBS <dianalorenabalanta@gmail.com>
2024-04-22 19:14:35 -0500

    feat:update_create_sale

--- src/sales/sales.service.ts
+++ src/sales/sales.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateSaleDto } from './dto/create-sale.dto';
 import { UpdateSaleDto } from './dto/update-sale.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -8,6 +8,8 @@ import { SaleDetail } from 'src/sale-details/entities/sale-detail.entity';
 import { Student } from 'src/roles/entities/student.entity';
 import { Restaurant } from 'src/roles/entities/restaurant.entity';
 import { Status } from './enum/status.enum';
+import { CreateSaleDetailDto } from 'src/sale-details/dto/create-sale-detail.dto';
+import { Product } from 'src/products/entities/product.entity';
 
 @Injectable()
 export class SalesService {
@@ -20,15 +22,58 @@ export class SalesService {
     private studentRepository: Repository<Student>,
     @InjectRepository(Restaurant)
     private restaurantRepository: Repository<Restaurant>,
+    @InjectRepository(Product)
+    private productRepository: Repository<Product>,
   ) {}
 
   async create(createSaleDto: CreateSaleDto): Promise<Sale> {
+    const { restaurantId, studentId, saleDetails } = createSaleDto;
+    const restaurant = await this.restaurantRepository.findOne({where:{id:restaurantId}});
+    const student = await this.studentRepository.findOne({where:{id:studentId}})
+    if (!restaurant) {
+      throw new NotFoundException('Restaurant no found.');
+    }
+    if (!student) {
+        throw new NotFoundException('Student no found.');
+    }
     const sale = new Sale();
-    sale.restaurant = await this.restaurantRepository.findOne({where:{id:createSaleDto.restaurantId}});
-    sale.student = await this.studentRepository.findOne({where:{id:createSaleDto.studentId}});
+    sale.restaurant = restaurant
+    sale.student = student
     sale.status = Status.PENDING;
+
+    let totalValue = 0;
+    const details = saleDetails.map(async detail => {
+        const saleDetail = new SaleDetail();
+        const product = await this.productRepository.findOne({where:{id:detail.productId}});
+        if(!product){
+          sale.status=Status.FAILED;
+          throw new NotFoundException('Product with id: ' + detail.productId + ' not found.');
+        }
+        saleDetail.product = product; // Replace with actual product retrieval logic
+        saleDetail.quantity = detail.quantity;
+        saleDetail.sale = sale; // link detail to sale
+        totalValue += saleDetail.subtotal; // sum up the total
+        return saleDetail;
+    });
+
+    sale.saleDetails = await Promise.all(details);
+
+    if (student.balance < totalValue) {
+      sale.status = Status.FAILED;
+      throw new BadRequestException('balance is not enough to complete the purchase.');
+    }
+
+    // Deduct from student, add to restaurant, complete the sale
+    student.balance -= totalValue;
+    restaurant.balance += totalValue;
+    sale.status = Status.COMPLETED;
+
+    this.studentRepository.save(student);
+    this.restaurantRepository.save(restaurant);
+    this.saleDetailRepository.save(sale.saleDetails);
+
     return this.saleRepository.save(sale);
-}
+  }
 
   async findAll() {
     return await this.saleRepository.find();
