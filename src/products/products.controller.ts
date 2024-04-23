a6995ee4d9469dd42ab4d0852e22ce730b1d8b05
Dan-Ale-Es-A <alexandraarenas1705@gmail.com>
2024-04-23 15:51:07 -0500

    fix:fixed_the_error_of_incorrectly_assigned_id_to_the_restaurant_when_creating_a_product

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -4,16 +4,26 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Auth } from 'src/auth/decorators/auth.decorators';
 import { Role } from 'src/roles/enum/role.enum';
+import { ActiveUser } from 'src/common/decorators/active-user.decorators';
+
 
 @Auth(Role.RESTAURANT)
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
-  @Post('restaurant/:userId') //Se cambiará cuando haya autorización para obtener el usuario logueado
+  /*@Post('restaurant/:userId') //Se cambiará cuando haya autorización para obtener el usuario logueado
   create(@Param('userId') userId:number, @Body() createProductDto: CreateProductDto) {
     return this.productsService.create(+userId, createProductDto);
   }
+  */ 
+  @Post()
+  create(@ActiveUser() user: any, @Body() createProductDto: CreateProductDto) {
+    // Suponiendo que el ID del restaurante está en user.restaurantId o similar.
+    console.log(user);
+    return this.productsService.create(user, createProductDto);
+  }
+  
   @Get()
   findAll() {
     return this.productsService.findAll();
