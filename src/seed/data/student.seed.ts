0175cffa8e27779bbafc632ff5cd865cd0594ffc
CarlosJaBr <carjabo12@gmail.com>
2024-04-23 16:03:07 -0500

    feat: add student  seed

---
+++ src/seed/data/student.seed.ts
@@ -0,0 +1,21 @@
+import { Student } from "src/roles/entities/student.entity";
+
+/*
+const studentSeed:Student[] = [
+    {
+        {
+            id:1,
+            email: 'supervisor@gmail.com',
+            password: 'Supervisor1',
+            balance:50000,
+            role:'student',
+            createdAt: new Date().getTime(),
+            deleteAt: null,
+            name: "Juana",
+            lastname: "Perez",
+            dni:11223344,
+            code: "A00",
+        }
+    }
+]
+*/
